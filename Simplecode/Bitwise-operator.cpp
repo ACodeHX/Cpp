23f233455c2b6e27eabfba6a4bedb5206c842158
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-19 15:52:51 +0800

    Add files via upload

---
+++ Simplecode/Bitwise-operator.cpp
@@ -0,0 +1,24 @@
+// Note:Your choice is C++ IDE
+#include "iostream.h"
+void main()
+{
+    int a,b,c;
+    a=60;
+    b=13;
+    c=0;
+    
+    c=a&b;
+    cout<<"a&b = "<<c<<"\n";
+    
+    c=a|b;
+    cout<<"a|b = "<<c<<"\n";
+    
+    c=a^b;
+    cout<<"a^b = "<<c<<"\n";
+    
+    c=a<<2;
+    cout<<"a<<2 = "<<c<<"\n";
+    
+    c=a>>2;
+    cout<<"a>>2 = "<<c<<"\n";
+}
