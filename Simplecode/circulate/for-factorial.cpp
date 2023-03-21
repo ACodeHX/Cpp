59187ff481f709c0e1f3616a7b9cb790aee5736e
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-21 15:27:42 +0800

    Add files via upload

---
+++ Simplecode/circulate/for-factorial.cpp
@@ -0,0 +1,19 @@
+// Note:Your choice is C++ IDE
+#include <iostream>//for求阶乘
+using namespace std;
+int main()
+{
+    int i,n=1,q;
+    cout<<"输入一个数:q"<<endl;
+    cin>>q;
+    for(i=1;i<=q;i++)
+    {
+    	
+    	n*=i;
+    	
+    }
+    cout<<q<<"! = ";
+    cout<<n<<endl;
+    
+    return 0;
+}
