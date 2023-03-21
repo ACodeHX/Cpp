fcbb766fd8539478c6b99582b09f6104287eb29b
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-21 12:50:37 +0800

    Add files via upload

---
+++ Simplecode/circulate/while-sum.cpp
@@ -0,0 +1,19 @@
+// Note:Your choice is C++ IDE
+#include <iostream>//while求和
+using namespace std;
+int main()
+{
+    int a=0,s=0,n;
+    
+    cout<<"求1到n的和"<<endl<<"输入n:";
+    
+    
+    cin>>n;
+    while(a<=n)
+    {
+    	s+=a;
+    	a++;
+    }
+    cout<<s<<endl;
+    return 0;
+}
