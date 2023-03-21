ce54684f31a0d24599e8bc423b2e161032ec130c
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-21 13:10:27 +0800

    Add files via upload

---
+++ Simplecode/circulate/for-sum.cpp
@@ -0,0 +1,19 @@
+// Note:Your choice is C++ IDE
+#include <iostream>//for求和
+using namespace std;
+int main()
+{
+    int a=0,s=0,n;
+    
+    cout<<"求1到n的和"<<endl<<"输入n:";
+    
+    
+    cin>>n;
+    for(a;a<=n;a++)
+    {
+    	s+=a;
+    	
+    }
+    cout<<s<<endl;
+    return 0;
+}
