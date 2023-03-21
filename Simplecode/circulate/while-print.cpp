37d305c1b3aa19bb57f61997a46a6d50c97212f4
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-21 12:42:42 +0800

    Add files via upload

---
+++ Simplecode/circulate/while-print.cpp
@@ -0,0 +1,15 @@
+// Note:Your choice is C++ IDE
+#include <iostream>//while´òÓ¡1~9µÄÊý
+using namespace std;
+int main()
+{
+    int a=0;
+    
+    while(a<10)
+    {
+    	cout<<a<<endl;
+    	a++;
+    }
+    
+    return 0;
+}
