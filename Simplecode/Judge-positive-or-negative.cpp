e7f70fcf1410368cf586b2bb2537d921e5175f7a
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-22 08:41:52 +0800

    Add files via upload

---
+++ Simplecode/Judge-positive-or-negative.cpp
@@ -0,0 +1,20 @@
+// Note:Your choice is C++ IDE
+#include <iostream>//do-while求阶乘
+using namespace std;
+void main()
+{
+   int i;
+   cout<<"判断正负"<<endl<<"输入一个数:";
+   cin>>i;
+   if(i>0)
+   {
+   	   cout<<i<<"是正数";
+   }else if(i<0)
+   {
+   	   cout<<i<<"是负数";
+   }else
+   {
+   	   cout<<i<<"是0";
+   }
+    
+}
