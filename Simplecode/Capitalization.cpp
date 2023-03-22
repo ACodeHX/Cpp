a258084a6d4b2ec33256ffa533d6f16d8683f7dc
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-22 08:36:24 +0800

    Add files via upload

---
+++ Simplecode/Capitalization.cpp
@@ -0,0 +1,25 @@
+// Note:Your choice is C++ IDE
+#include <iostream>//大小写转换
+using namespace std;
+int main()
+{
+    int i;
+    cin>>i;
+    if(i<97&&i>=65)
+    {
+    	cout<<char(i)<<"的小写";
+        i+=32;
+        cout<<char(i);
+    }
+    else if(i<123&&i>=97)
+    {
+    	cout<<char(i)<<"的大写";
+        i-=32;
+        cout<<char(i);
+    }else
+    {
+    	cout<<"请输入65~122的数";
+    }
+    
+    return 0;
+}
