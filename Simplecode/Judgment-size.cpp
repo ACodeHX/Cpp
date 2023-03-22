371be26cd8a8d09b635f011505e6461806f2e75f
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-22 08:45:13 +0800

    Add files via upload

---
+++ Simplecode/Judgment-size.cpp
@@ -0,0 +1,18 @@
+// Note:Your choice is C++ IDE
+#include <iostream>//§Ø?§³
+using namespace std;
+void main()
+{
+    int i=0,j=0,n=0;
+    cin>>i>>j;
+    if(i>=j)
+    {
+    n=i;
+    }
+    else 
+    {
+    n=j;
+    }
+    cout<<n;
+    
+}
