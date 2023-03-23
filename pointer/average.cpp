7aab33b3fffca01e07671074e8bdacb060c33cf0
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-23 17:30:35 +0800

    Add files via upload

---
+++ pointer/average.cpp
@@ -0,0 +1,28 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+double getavg(int *arr,int size);  //函数声明
+
+
+
+int main()
+{
+    int balance[4]={10,88,6,44};   //数组定义
+    //cout<<balance;
+    double avg;
+    avg=getavg(balance,4);
+    cout<<"平均数 = "<<avg<<endl;
+    return 0;
+    
+}
+double getavg(int *arr,int size)
+{
+	int i,sum=0;
+	double avg;
+	for(i=0;i<size;i++)
+	{
+		sum+=arr[i];	
+	}
+	avg=double(sum)/size;
+	return avg;
+}
