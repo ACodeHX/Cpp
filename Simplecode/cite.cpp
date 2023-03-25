a560be1e7598a952eaed1e31aee127545532bb29
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-25 15:21:16 +0800

    Add files via upload

---
+++ Simplecode/cite.cpp
@@ -0,0 +1,21 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+void main()    //C++µÄÒıÓÃ
+{
+    int i;
+    double d;
+    
+    int& s=i;
+    double& r=d;
+    
+    i=10;
+    cout<<i<<endl;
+    cout<<s<<endl;
+    
+    d=2.2;
+    cout<<d<<endl;
+    cout<<r<<endl;
+    
+
+}
