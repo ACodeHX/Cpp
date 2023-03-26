8cb8c20116345ffc4bf414598ed891c84965823f
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-03-26 18:10:10 +0800

    Add files via upload

---
+++ Simplecode/max-min.cpp
@@ -0,0 +1,33 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+void main()
+{
+	int a,b,c,max,min;
+	//cin>>a>>b>>c>>endl;
+	cout<<"输入三个数"<<endl;
+	
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	
+	if(a>=b)
+	max=a;
+	else max=b;
+	if(c>=max)
+	max=c;
+	
+	
+	cout<<"最大值max = "<<max<<endl;
+	
+	if(a>b)
+	min=b;
+	else min=a;     
+	if(min>c)
+	min=c;
+	
+	cout<<"最小值min = "<<min<<endl;
+	cout<<a <<b <<c ;
+    
+   
+}
