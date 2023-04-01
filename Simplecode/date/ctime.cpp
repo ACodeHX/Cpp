9097e58c2219980bd3ba52e70c775691925caae8
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-01 20:13:17 +0800

    Add files via upload

---
+++ Simplecode/date/ctime.cpp
@@ -0,0 +1,12 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+#include <time.h>
+using namespace std;
+int main()    //利用ctime获得当前时间
+{
+    time_t curtime;
+    time(&curtime);
+    printf("当前时间:%s\n",ctime(&curtime));
+    
+    
+}
