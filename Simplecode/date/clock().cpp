8c909e1cc7898a241e54eba1faa07d618d4ff95f
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-02 17:09:00 +0800

    Update clock().cpp

--- Simplecode/date/clock().cpp
+++ Simplecode/date/clock().cpp
@@ -9,15 +9,15 @@ void main()    //clock()函数的作用
     int i;
     
     start_t=clock_t();
-    printf("程序启动:start_t=%ld\n",start_t);
-    printf("开始大循环:%ld\n",start_t);
+    printf("程序启动:start_t = %ld\n",start_t);
+    printf("开始大循环:start_t = %ld\n",start_t);
     for(i=0;i<10000000;i++)
     {
     }
     end_t=clock();
-    printf("结束大循环:%ld\n",end_t);
+    printf("结束大循环:end_t = %ld\n",end_t);
     
     tatol_t=(double)(end_t-start_t)/CLOCKS_PER_SEC;    //CLOCKS_PER_SEC等于1000000，该函数大约每 72 分钟会返回相同的值。
-    printf("CPU占用时间:%f\n",tatol_t);    //数据类型不要错
+    printf("CPU占用时间:tatol_t = %f\n",tatol_t);    //数据类型不要错
     cout<<"结束进程"<<endl;
 }
