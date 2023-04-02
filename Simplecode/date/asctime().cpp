94c15cfc9664a2081c8c5b143d26ac498cfefb39
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-02 19:16:04 +0800

    Add files via upload

---
+++ Simplecode/date/asctime().cpp
@@ -0,0 +1,21 @@
+// Note:Your choice is C++ IDE
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main()    //asctime()ÓÃ·¨
+{
+   struct tm t;
+
+   t.tm_sec    = 3;
+   t.tm_min    = 11;
+   t.tm_hour   = 8;
+   t.tm_mday   = 28;
+   t.tm_mon    = 2;
+   t.tm_year   = 89;
+   t.tm_wday   = 6;
+
+   puts(asctime(&t));
+   
+   return(0);
+}
