5f25f6fb289ac8bb4756f1fad0ac2ae04b530849
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-15 18:38:40 +0800

    Add files via upload

---
+++ object-oriented/class.cpp
@@ -0,0 +1,46 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+ 
+using namespace std;
+ 
+// 基类
+class Shape 
+{
+   public:
+      void setWidth(int w)
+      {
+         width = w;
+      }
+      void setHeight(int h)
+      {
+         height = h;
+      }
+   protected:
+      int width;
+      int height;
+};
+ 
+// 派生类
+class Rectangle: public Shape
+{
+   public:
+      int getArea()
+      { 
+         return (width * height); 
+      }
+};
+ 
+int main(void)
+{
+	int i,n;
+	scanf("%d %d",&i,&n);
+   Rectangle Rect;
+ 
+   Rect.setWidth(i);
+   Rect.setHeight(n);
+ 
+   // 输出对象的面积
+   cout << "Total area: " << Rect.getArea() << endl;
+ 
+   return 0;
+}
