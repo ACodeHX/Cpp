e7ca3516e90d5c0c0b6cb5b65f30ec1b93b805d0
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-20 19:58:39 +0800

    Add files via upload

---
+++ Simplecode/bool.cpp
@@ -0,0 +1,15 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+void main()
+{
+    bool feal = true;
+    
+    cout<<"true = "<<feal<<endl;
+    
+    feal = false;
+    
+    cout<<"false = "<<feal<<endl;
+    
+    cout<<"bool size = "<<sizeof(bool);
+}
