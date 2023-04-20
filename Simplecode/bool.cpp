0e83a2ce3baa5317ddd9e8462211ca971b06e1dc
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-20 20:00:45 +0800

    Update bool.cpp

--- Simplecode/bool.cpp
+++ Simplecode/bool.cpp
@@ -1,15 +1,14 @@
-// Note:Your choice is C++ IDE
 #include <iostream>
 using namespace std;
-void main()
+void main()    //bool类型
 {
     bool feal = true;
     
-    cout<<"true = "<<feal<<endl;
+    cout<<"true = "<<feal<<endl;    //返回 1
     
     feal = false;
     
-    cout<<"false = "<<feal<<endl;
+    cout<<"false = "<<feal<<endl;    //返回 0
     
-    cout<<"bool size = "<<sizeof(bool);
+    cout<<"bool size = "<<sizeof(bool);    //类型大小 1
 }
