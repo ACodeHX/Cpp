2e1f7bdcc97eae9ab1cba743903642c68456b600
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-21 21:21:13 +0800

    Add files via upload

---
+++ Simplecode/operator.cpp
@@ -0,0 +1,19 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+void main()
+{
+    int a = 60,b = 15;    //60的二进制为111100; 15的二进制为001111;
+        cout<<(a+b)<<endl;
+        cout<<(a-b)<<endl;
+        cout<<(a*b)<<endl;
+        cout<<(a%b)<<endl;
+        cout<<(a/b)<<endl;
+        cout<<(a^b)<<endl;
+        cout<<(a&b)<<endl;
+        cout<<(a>>b)<<endl;
+        cout<<(a<<b)<<endl;
+        cout<<(a!=b)<<endl;
+        cout<<(a==b)<<endl;
+        cout<<(a|b)<<endl;
+}
