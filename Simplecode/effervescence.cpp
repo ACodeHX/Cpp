420868864ddbfec7b10c4fa079ca8a72b051a8d2
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-24 17:36:44 +0800

    Add files via upload

---
+++ Simplecode/effervescence.cpp
@@ -0,0 +1,37 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {1,6,3,8,5};
+    int i = 0,j = 0;
+    cout << "Ã°ÅÝÅÅÐòÇ°:"<<endl;
+    for(i=0;i<5;i++)
+    {
+        cout << arr[i];
+    }
+    cout << endl;
+    int start = 0;
+    int end = sizeof(arr)/sizeof(arr[0])-1;
+    int temp = 0;
+
+    for(i=0;i<end;i++)
+    {
+        for(j=0;j<end-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    cout<<"ÅÅÐòºó:"<<endl;
+    for(i=0;i<5;i++)
+    {
+        cout<<arr[i];
+    }
+    return 0;
+}
