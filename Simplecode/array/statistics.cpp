44a65081013dc5f928420088c2f774d76e534e31
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-25 21:49:24 +0800

    Update statistics.cpp

--- Simplecode/array/statistics.cpp
+++ Simplecode/array/statistics.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void main()
+void main()    //统计
 {
     int arr[3][3] =
     {
         {100,127,134},
         {94,105,126},
         {98,100,99}
-    };
+    };    //定义数组
     string name[3] ={"tom","ACHX","jek"}; 
     for(int i = 0;i<3;i++)
     {
