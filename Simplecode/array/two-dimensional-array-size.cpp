2164c947dce9c3c0bf96e15332621cc8be244e89
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-25 20:56:15 +0800

    Add files via upload

---
+++ Simplecode/array/two-dimensional-array-size.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+    int arr[2][3] =
+    {
+        {1,2,3},{4,5,6}
+    };
+    cout<<"数组的大小"<<sizeof(arr)<<endl;
+    cout<<"第一列数组元素的大小:"<<sizeof(arr[0])<<endl;
+    cout<<"第二列数组的大小:"<<sizeof(arr[1])<<endl;
+    cout<<sizeof(arr[0])/sizeof(arr[0][0])<<endl;
+
+    cout<<"数组的地址:"<<(int)arr<<endl;
+    cout<<"第一列数组的地址:"<<(int)arr[0]<<endl;
+    cout<<"第二列数组的地址:"<<(int)arr[1]<<endl;
+    cout<<"数组首地址:"<<(int)&arr[0][0];
+}
