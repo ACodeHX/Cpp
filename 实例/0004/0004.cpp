1036735c7efd3c371c8b5b4ac5cf153c3f6c5d33
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-28 21:58:13 +0800

    Add files via upload

---
+++ 实例/0004/0004.cpp
@@ -0,0 +1,22 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+void sawp(int num1,int num2)
+{
+    int temp;
+    cout<<num1<<endl;
+    cout<<num2<<endl;
+
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    cout<<num1<<endl;
+    cout<<num2<<endl;
+}
+void main()    //
+{
+    int a=10;
+    int b=20;
+    sawp(a,b);
+}
