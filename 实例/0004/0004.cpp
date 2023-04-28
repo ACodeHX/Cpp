06c1ea1d3221b97f0277b0453fe3eee33110e13f
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-28 22:01:37 +0800

    Update 0004.cpp

--- 实例/0004/0004.cpp
+++ 实例/0004/0004.cpp
@@ -3,15 +3,15 @@ using namespace std;
 void sawp(int num1,int num2)    //练习调用
 {
     int temp;
-    cout<<num1<<endl;
-    cout<<num2<<endl;
+    cout<<"交换前a="<<num1<<endl;
+    cout<<"交换前b="<<num2<<endl;
 
     temp = num1;
     num1 = num2;    //交换
     num2 = temp;
 
-    cout<<num1<<endl;
-    cout<<num2<<endl;
+    cout<<"交换后a="<<num1<<endl;
+    cout<<"交换后b="<<num2<<endl;
 }
 void main()    //
 {
