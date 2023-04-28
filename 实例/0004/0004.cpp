f14acaa9eade52b3331b585ba12b6ffb833b71d9
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-04-28 22:03:40 +0800

    Update 0004.cpp

--- 实例/0004/0004.cpp
+++ 实例/0004/0004.cpp
@@ -13,9 +13,13 @@ void sawp(int num1,int num2)    //练习调用
     cout<<"交换后a="<<num1<<endl;
     cout<<"交换后b="<<num2<<endl;
 }
-void main()    //
+void main()
 {
-    int a=10;
-    int b=20;
+    int a=0;
+    int b=0;
+    cout<<"输入a的值:"<<endl;
+    cin>>a;
+    cout<<"输入b的值:"<<endl;
+    cin>>b;
     sawp(a,b);
 }
