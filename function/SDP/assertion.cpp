d287f48e3bb52598e89b389c561b5205b4a54f93
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-01 21:48:52 +0800

    Add files via upload

---
+++ function/SDP/assertion.cpp
@@ -0,0 +1,17 @@
+/* Note:Your choice is C IDE */
+#include "assertion.h"
+int add(int a, int, b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	cout << a << endl;
+	cout << b << endl;
+}
+void main()
+{
+	int a = 10;
+	int b = 20;
+	add(a, b);
+}
---
+++ function/SDP/assertion.h
@@ -0,0 +1,4 @@
+/* Note:Your choice is C IDE */
+#include <iostream>
+using namespace std;
+int add(int a,int b);
