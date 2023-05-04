8bd090822fb249d22891680bbce32530188cbcc6
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-04 12:25:15 +0800

    Add files via upload

---
+++ 实例/0007/0007.cpp
@@ -0,0 +1,16 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int x=0, y=0, z=0, i=0;
+	for (i = 100; i < 1000; i++)
+	{
+		x = i % 10;
+		y = i /10% 10;
+		z = i / 100 % 10;
+		if (i == (x*x * x + y*y * y + z*z * z))
+			cout << i<< endl;
+	}
+}
