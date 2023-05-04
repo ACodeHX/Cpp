4fa27833753abae672315f5c8f71674804cc5b71
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-04 12:20:36 +0800

    Add files via upload

---
+++ 实例/0006/0006.cpp
@@ -0,0 +1,17 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int x, y, result;
+	for (x = 1; x < 10; x++)
+	{
+		for (y = 1; y <= x; y++)
+		{
+			result = x * y;
+			cout << x << "*" << y << "=" << result << "\t";
+		}
+		cout << endl;
+	}
+}
