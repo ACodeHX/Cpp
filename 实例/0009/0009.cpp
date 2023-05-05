d53b56e0796c676e3a98838efefa1995f2d0f0d5
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-05 19:55:09 +0800

    Update 0009.cpp

--- 实例/0009/0009.cpp
+++ 实例/0009/0009.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-void main()
+void main()    //输入一行字符串，统计出其中英文字母、空格、数字和其它字符的个数
 {
 	char c;
 	int letters = 0, spaces = 0, digits = 0, others = 0;
-	cout << "" << endl;
+	cout << "输入字符串:" << endl;
 	while ((c=getchar()) != '\n')
 	{
 		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
