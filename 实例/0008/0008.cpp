d8161eeb122540f485375e28bba09c8bf8fe7647
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-05 19:48:55 +0800

    Update 0008.cpp

--- 实例/0008/0008.cpp
+++ 实例/0008/0008.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
-void main()
+void main()    //利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示
 {
     int score;
     char grade;
-    cout << "" << endl;
+    cout << "输入分数" << endl;
     cin >> score;
     if (score >= 0 && score <= 100)
     {
         grade = (score >= 90) ? 'A' : ((score >= 60) ? 'B' : 'C');
         cout << grade << endl;
     }
-    else cout << "error" << endl;
+    else cout << "请输入0~100的数" << endl;
 }
