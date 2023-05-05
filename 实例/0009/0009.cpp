17328189e2a0c562d8b285b10b42674368b9e6d1
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-05 19:53:14 +0800

    Add files via upload

---
+++ 实例/0009/0009.cpp
@@ -0,0 +1,22 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	char c;
+	int letters = 0, spaces = 0, digits = 0, others = 0;
+	cout << "" << endl;
+	while ((c=getchar()) != '\n')
+	{
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+			letters++;
+		else if (c >= '0' && c <= '9')
+			spaces++;
+		else if (c == ' ')
+			digits++;
+		else
+			others++;
+	}
+	cout << letters << spaces <<digits<< others << endl;
+}
