2941eade37cf7fb075fa203ee6809ad8b3502ae7
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-06 23:04:20 +0800

    Add files via upload

---
+++ 实例/0011/0011.cpp
@@ -0,0 +1,17 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    float h, s;
+    int i = 0;
+    h = s = 100;
+    h = h / 2; //第一次反弹高度
+    for (i = 2; i <= 10; i++)
+    {
+        s = s + 2 * h;
+        h = h / 2;
+    }
+    printf("第10次落地时，共经过%f米，第10次反弹高%f米\n", s, h);
+}
