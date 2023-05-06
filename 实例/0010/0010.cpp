1cb506f7ec7fb53601532cc2980c7e45e17b5e54
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-06 09:41:06 +0800

    Update 0010.cpp

--- 实例/0010/0010.cpp
+++ 实例/0010/0010.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-void main()
+void main()    //求s=a+aa+aaa+aaaa+aa...a的值
 {
 	int s = 0, a, n, t;
-	cout << "" << endl;
+	cout << "请输入a和n" << endl;
 	cin >> a;
 	cin >> n;
 	t = a;
@@ -14,5 +14,5 @@ void main()
 		t += a;
 		n--;
 	}
-	cout << "" << s << endl;
+	cout << "a+aa+...=" << s << endl;
 }
