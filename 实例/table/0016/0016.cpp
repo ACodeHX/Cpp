3fc0380e173ef467bb69d98b83f7944035688556
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-13 16:06:04 +0800

    Add files via upload

---
+++ 实例/table/0016/0016.cpp
@@ -0,0 +1,21 @@
+// Note:Your choice is C++ IDE
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int num, i;
+	num = 3;
+	for (i = 0; i < 3; i++)
+	{
+		cout << ""<<num<<endl;
+		num++;
+			for (i = 0; i < 3; i++)
+			{
+				auto num = 1;
+				cout << "ÄÚÖÃ";
+				num++;
+					cout << num<<endl;
+			}
+	}
+}
