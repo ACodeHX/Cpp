aaae09ed60f604152f30b5e779cef9d9dc1f6367
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-22 13:48:10 +0800

    Add files via upload

---
+++ 实例/table/0020/0020.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <sstream>
+using namespace std;
+int main()
+{
+	string input;
+	cout << "输入一串数字和空格:" << endl;
+	getline(cin, input);    // 读取用户输入的整行数据
+	istringstream iss(input);    // 将字符串转换为输入流
+
+	int num , sum = 0;
+
+	while (iss>> num)
+	{
+		sum = sum + num;
+	}
+	cout << sum << endl;
+	return 0;
+}
