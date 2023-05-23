58445d276d5f441e605fd60b4aee24e12b448f5d
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-23 10:01:26 +0800

    Add files via upload

---
+++ object-oriented/函数的重载.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;    //函数的重载
+
+void freigth(int a);
+void freigth(float b);
+void freigth(int, float);
+void main()    //返回类型不同，但是传递给这两个函数的参数个数和数据类型都相同
+{
+	int a = 5;
+	float b = 3.6;
+
+	freigth(a);
+	freigth(b);
+	freigth(a, b);
+}
+
+void freigth(int var)
+{
+	cout << var << endl;
+}
+void freigth(float var)
+{
+	cout << var << endl;
+}
+void freigth(int var, float var1)
+{
+	cout << var << endl;
+	cout << var1 << endl;
+}
