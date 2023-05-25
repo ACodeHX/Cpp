23cea2564cd829c1cbc54376f5bc7d3ecd005abc
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-25 19:46:59 +0800

    Add files via upload

---
+++ 实例/table/0023/0023.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	long long a = 1, b = 2, temp=0,sum=0;
+	while (b <= 4e7)
+	{
+		if (a % 2 == 0)
+		{
+			sum += a;
+		}
+		temp = a + b;
+		a = b;
+		b = temp;
+		
+	}
+	cout << sum << endl;
+}
