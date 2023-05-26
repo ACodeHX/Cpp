5640686c327d27ae63b3f062f7b708504090bad5
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-26 22:49:16 +0800

    Add files via upload

---
+++ 实例/table/0024/0024.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0, reversal = 0, temp = 0;
+	cout << "反转数字:" << endl;
+	cin >> n;
+	while (n != 0)
+	{
+		reversal = n%10;
+		temp = temp*10+reversal;
+		n /= 10;
+	}
+	cout << temp;
+	return 0;
+}
