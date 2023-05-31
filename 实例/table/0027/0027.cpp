d3cbd75d4a3eb1a4c314109f1ad7eeed7e5fb43d
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-31 21:59:54 +0800

    Update 0027.cpp

--- 实例/table/0027/0027.cpp
+++ 实例/table/0027/0027.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main() {
     long long num = 600851475143;
     int maxPrime = 1;
-
+    //从2开始逐个测试每个数是否是给定数的因数
     for (int i = 2; i <= num / i; i++) {
         while (num % i == 0) {
             maxPrime = i;
-            num /= i;
+            num /= i;    //将num除以i,继续寻找下一个质因数
         }
     }
 
-    if (num > 1) {
+    if (num > 1) {    //如果num本身是质数,将num作为最大质因数
         maxPrime = num;
     }
 
