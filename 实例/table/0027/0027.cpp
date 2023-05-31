76fb126ea14efba9b468fff9383a8670c0ce3b30
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-05-31 21:43:17 +0800

    Add files via upload

---
+++ 实例/table/0027/0027.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    long long num = 600851475143;
+    int maxPrime = 1;
+
+    for (int i = 2; i <= num / i; i++) {
+        while (num % i == 0) {
+            maxPrime = i;
+            num /= i;
+        }
+    }
+
+    if (num > 1) {
+        maxPrime = num;
+    }
+
+    cout << "The largest prime factor of 600851475143 is: " << maxPrime << endl;
+    return 0;
+}
