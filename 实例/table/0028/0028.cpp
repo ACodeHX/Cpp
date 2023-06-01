548003cb70250c411b9054edb2dc113b9a4c5921
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-06-01 20:05:17 +0800

    Add files via upload

---
+++ 实例/table/0028/0028.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(int* s1, int* s2)
+{
+	int temp;
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
+
+int main()
+{
+	int x, y, z;
+	int *p1,*p2,*p3;
+	scanf_s("%d", &x);
+	scanf_s("%d", &y);
+	scanf_s("%d", &z);
+	p1 = &x;
+	p2 = &y;
+	p3 = &z;
+	if (x > y)
+		swap(p1, p2);
+	if (x > z)
+		swap(p1, p3);
+	if (y > z)
+		swap(p2, p3);
+	cout << x << y << z;
+}
