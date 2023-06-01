6f8b327e137ef393db0c7bbf95a331131d7c2ae3
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-06-01 20:08:05 +0800

    Update 0028.cpp

--- 实例/table/0028/0028.cpp
+++ 实例/table/0028/0028.cpp
@@ -6,14 +6,14 @@ void swap(int* s1, int* s2)
 	int temp;
 	temp = *s1;
 	*s1 = *s2;
-	*s2 = temp;
+	*s2 = temp;    //交换指针
 }
 
 int main()
 {
 	int x, y, z;
 	int *p1,*p2,*p3;
-	scanf_s("%d", &x);
+	scanf_s("%d", &x);    //安全输出
 	scanf_s("%d", &y);
 	scanf_s("%d", &z);
 	p1 = &x;
