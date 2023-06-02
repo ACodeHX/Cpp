6250cf17f8207d7affc4784a53298edcdf6d6a28
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-06-02 19:30:49 +0800

    Add files via upload

---
+++ 实例/table/0029/0029.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int str;
+	str = getchar();    //获得第一个数
+	putchar(str);    //输出
+
+	printf("\n");
+	putchar(-1);    //输入乱码
+	printf("\n");
+	putchar(256);    //什么也没输出
+
+	int str1;
+	str1 = getchar();
+	printf("输入 ctrl+C 结束");
+	while (str1 != EOF)    //这个可以输出输入的数据
+	{
+		putchar(str1);
+		str1 = getchar();
+	}
+	return 0;
+}
