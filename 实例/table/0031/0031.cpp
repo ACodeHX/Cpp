ad35e93722842566fdf12b14f85c42d2cccfc85a
ACodeHX <127362983+ACodeHX@users.noreply.github.com>
2023-06-07 16:04:11 +0800

    Add files via upload

---
+++ 实例/table/0031/0031.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+long long multiple(int start, int end) {
+    long long number = end;
+
+    while (true) {
+        bool isDivisible = true;
+        for (int i = start; i <= end; i++) {
+            if (number % i != 0) {
+                isDivisible = false;
+                break;
+            }
+        }
+
+        if (isDivisible) {
+            return number;
+        }
+
+        number += end;
+    }
+}
+
+int main() {
+    int start = 1;
+    int end = 20;
+
+    long long smallestMultiple = multiple(start, end);
+
+    cout << "最小的能够被 " << start << " 到 " << end << " 整除的正数是：" << smallestMultiple << endl;
+
+    return 0;
+}
